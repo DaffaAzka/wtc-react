12c7389e67c513406a11b7b6bb7f91d9288d129a
Azka <azkadaiki26@gmail.com>
2026-08-05 14:58:46 +0700

    feat: add rich editor components and optimize it

---
+++ app/components/editor/nav-modules.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import { Link } from "react-router";
+
+export function NavModules({
+  items,
+}: {
+  items: {
+    name: string;
+    url: string;
+    icon: React.ReactNode;
+    roleAllowed?: boolean;
+  }[];
+}) {
+  const { isMobile } = useSidebar();
+
+  return (
+    <SidebarGroup className="group-data-[collapsible=icon]:hidden">
+      <SidebarGroupLabel>Management</SidebarGroupLabel>
+      <SidebarMenu>
+        {items
+          .filter((module) => module.roleAllowed)
+          .map((item) => (
+            <div key={item.name}>
+              <SidebarMenuItem key={item.name}>
+                <SidebarMenuButton asChild>
+                  <Link to={item.url}>
+                    {item.icon}
+                    <span>{item.name}</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </div>
+          ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  );
+}
