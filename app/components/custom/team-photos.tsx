c4f1c5f161aa6c54152b5f771d827f2ce231d242
Raiyll <ahmadrafifadilah50@gmail.com>
2026-08-23 14:15:31 +0700

    add: photos

--- app/components/custom/team-photos.tsx
+++ app/components/custom/team-photos.tsx
@@ -1,1 +1,22 @@
-export const teamPhotos = ["/images/team/team1.jpeg", "/images/team/team2.jpeg", "/images/team/team3.jpeg", "/images/team/team4.jpeg", "/images/team/team5.jpeg", "/images/team/team6.png"];
+export const teamPhotos = [
+  "/images/team/team1.jpeg",
+  "/images/team/team2.jpeg",
+  "/images/team/team3.jpeg",
+  "/images/team/team4.jpeg",
+  "/images/team/team5.jpeg",
+  "/images/team/team6.png",
+  "/images/team/team7.png",
+  "/images/team/team8.png",
+  "/images/team/team9.png",
+  "/images/team/team10.png",
+  "/images/team/team11.png",
+  "/images/team/team12.png",
+  "/images/team/team13.png",
+  "/images/team/team14.png",
+  "/images/team/team15.png",
+  "/images/team/team16.png",
+  "/images/team/team17.png",
+  "/images/team/team18.jpeg",
+  "/images/team/team19.jpeg",
+  "/images/team/team20.jpeg",
+];
